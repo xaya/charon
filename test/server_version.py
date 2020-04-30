d2381d0ee8f17ae3e6355a52a78d17f4dd0e76f0
Daniel Kraft <d@domob.eu>
2020-04-30 11:08:56 +0200

    Integration test backend version selection.
    Add a new integration test that runs two servers at the same base JID
    but with two versions, and verifies that two clients requesting one of the
    versions each get the right server discovered.

---
+++ test/server_version.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+
+#   Charon - a transport system for GSP data
+#   Copyright (C) 2020  Autonomous Worlds Ltd
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Tests selection of the right server if two versions are provided.
+"""
+
+import testcase
+
+
+class Methods:
+
+  def __init__ (self, val):
+    self.returnValue = val
+
+  def test (self):
+    return self.returnValue
+
+
+right = Methods ("right")
+left = Methods ("left")
+
+with testcase.Fixture (["test"]) as t, \
+     t.runServer (right, extraArgs=["--backend_version", "right"]), \
+     t.runServer (left, extraArgs=["--backend_version", "left"]), \
+     t.runClient (extraArgs=["--backend_version", "right"]) as cr, \
+     t.runClient (extraArgs=["--backend_version", "left"]) as cl:
+  t.assertEqual (cr.rpc.test (), "right")
+  t.assertEqual (cl.rpc.test (), "left")
